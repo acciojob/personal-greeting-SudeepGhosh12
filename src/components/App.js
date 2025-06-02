cf1f9d828531755b8f1de921aabdf548be11e282
Sudeep Ghosh <133862916+SudeepGhosh12@users.noreply.github.com>
2025-06-02 14:28:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input,setInput] = useState('');
+  const onInput = (e)=>{
+      const {value} = e.target
+    setInput(value)
+  }
+  const onClear = ()=>{
+    setInput('')
+  }
+  
   return (
     <div>
-        {/* Do not remove the main div */}
-        <input
-        type="text"
-        placeholder="Enter your name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      {name && <p>Hello, {name}!</p>}
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
   )
 }
